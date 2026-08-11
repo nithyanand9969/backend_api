5007107d912d493a1f62acab912ec1b94396504a
nithyanand9969 <nithyanandnadar.m@gmail.com>
2026-08-11 15:44:49 +0530

    testing

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,16 +1,38 @@
 module Api
     module V1
         class ChallengesController < ApplicationController
-            
+
             def index
-            end
-            def show
+               
+                @challenges =  Challenge.all
+                render json: @challenges
             end
             def create
+                puts "rrrr"
+                puts params
+                puts 'wwwwww'
+                challenge = Challenge.new(title:'welcome',description:'welcome to the challenge',start_date:Date.today,end_date:Date.today+10)
+                if challenge.save
+                    render json: {
+                        status: 'success',
+                        message: 'Challenge created successfully',
+                        data: challenge
+                    }
+                else render json:{
+                    status: 'error',
+                    message: 'Challenge not created',
+                }
+                            end
+                        end
+            def show
+                #show single challenge
+               
             end
             def update
+                #update single challenge
             end
             def destroy
+                #delete single challenge
             end
 
            
